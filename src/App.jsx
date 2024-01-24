95a7aa80c815240aac8d38b73b489fe1122e8ce1
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-01-24 00:08:34 -0500

    Mise en place de l'architecture Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./global.css";
+import s from "./style.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={s.main_container}>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>Logo</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <input style={{width:"100%"}} type="text" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className={s.tv_show_detail}>Detail</div>
+      <div className={s.recommendations}>Recommendation</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
